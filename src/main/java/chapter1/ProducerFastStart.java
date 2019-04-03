fd7cfb468981dfe6143d1ed81b917f7031e27c84
lianwei <lianwei1994@163.com>
2019-04-03 23:46:50 +0800

    测试kafka的使用

--- src/main/java/chapter1/ProducerFastStart.java
+++ src/main/java/chapter1/ProducerFastStart.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 
 /**
  * 代码清单1-1
- * Created by 朱小厮 on 2018/7/21.
+ * Created by IThawk on 2019/4/3.
  */
 public class ProducerFastStart {
     public static final String brokerList = "localhost:9092";
@@ -15,20 +15,16 @@ public class ProducerFastStart {
 
     public static void main(String[] args) {
         Properties properties = new Properties();
-        properties.put("key.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
-        properties.put("value.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.put("bootstrap.servers", brokerList);
 
-
-        KafkaProducer<String, String> producer =
-                new KafkaProducer<>(properties);
-        ProducerRecord<String, String> record =
-                new ProducerRecord<>(topic, "hello, Kafka!");
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "hello, Kafka!");
         try {
             producer.send(record);
 //            producer.send(record).get();
+            System.out.println("发送消息");
         } catch (Exception e) {
             e.printStackTrace();
         }
